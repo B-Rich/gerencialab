cba6790632b1414d222829b1bceade0ed84822c8
Gutierrez PS <gutierrez@live.jp>
2015-12-01 12:49:37 -0300

    Hotfix to base URL

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/gerencialab';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
